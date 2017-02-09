fda4c1a8825a157c38e7822fffe61d94e8042c19
Darius Korzeniewski <dariuskorzeniewski@dariuss-macbook.local>
2017-02-09 21:51:14 +0100

    Initial Commit

---
+++ MeinSpiel/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  MeinSpiel
+//
+//  Created by Darius Korzeniewski on 09.02.17.
+//  Copyright © 2017 Darius Korzeniewski. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
